c47e155ad3e15325d85cdd3674e0caee6a1115b4
Sang Phạm Tài <phamtaisang97@gmail.com>
2019-05-28 15:54:14 +0700

    h

---
+++ BTL_NET2/Controllers/TimKiemController.cs
@@ -0,0 +1,34 @@
+﻿using BTL_NET2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTL_NET2.Controllers
+{
+    public class TimKiemController : Controller
+    { 
+        // GET: TimKiem
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult TimKiem(string searchString)
+        {
+            Model1 db = new Model1();
+            //ViewBag.timkiem = searchString;
+            var links = from l in db.PRODUCT // lấy toàn bộ liên kết
+                        select l;
+
+            if (!String.IsNullOrEmpty(searchString)) // kiểm tra chuỗi tìm kiếm có rỗng/null hay không
+            {
+                links = links.Where(s => s.name.Contains(searchString)); //lọc theo chuỗi tìm kiếm
+            }
+
+            ViewBag.show = links;
+            return View();
+        }
+    }
+}
